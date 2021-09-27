6d77c5dabf5377cec8404f71c65e81f05bfb0e22
sohaibnabeel15 <47370480+sohaibnabeel15@users.noreply.github.com>
2021-09-27 17:13:20 -0600

    Add files via upload

---
+++ TIme Machine/150Million_BC.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TIme_Machine
+{
+    /// <summary>
+    /// Interaction logic for _150Million_BC.xaml
+    /// </summary>
+    public partial class _150Million_BC : Page
+    {
+        public _150Million_BC()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TIme Machine/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Time_Machine
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Page
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+            Select_Year year_page = new Select_Year();
+            this.NavigationService.Navigate(year_page);
+        }
+
+        private void Button_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+
+        }
+    }
+}
